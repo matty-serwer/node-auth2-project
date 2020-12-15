03307b392dc0759898bb83c7e013b1bc528ae50d
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-12-15 17:14:55 -0500

    Used knex to create database

---
+++ database/migrations/20201215170411_users.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('users', table => {
+      table.increments();
+      table.string("username", 128).notNullable().unique().index();
+      table.string("password", 256).notNullable();
+      table.string("department", 256).notNullable().defaultTo('mail-room');
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
